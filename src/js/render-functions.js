82c7baca4b55cb5cac972bb5a4b3237c73a97dbe
Artem <artemlotockij@gmail.com>
2026-05-03 20:48:26 +0200

    Fixes JS

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,20 +1,26 @@
+import { getImagesByQuery } from "./pixabay-api";
+
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 
 function createGallery(images) {
-    return images.map(({ webformatURL, previewURL, tags, likes, views, comments, downloads }) =>
-        `
-    <li class="gallery-item">
-       <a class="gallery-link" href=${webformatURL}><img class="gallery-img" src=${previewURL} alt=${tags}/></a>
-       <ul class="gallery-list">
-         <li><p>Likes ${likes}</p></li>
-         <li><p>Views ${views}</p></li>
-         <li><p>Comments ${comments}</p></li>
-         <li><p>Downloads ${downloads}</p></li>
-       </ul>
-    </li>
+  const markup = images.map(
+    ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-img" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <ul class="gallery-list">
+          <li><p>Likes ${likes}</p></li>
+          <li><p>Views ${views}</p></li>
+          <li><p>Comments ${comments}</p></li>
+          <li><p>Downloads ${downloads}</p></li>
+        </ul>
+      </li>
     `
-    ).join("");
+  ).join("");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
 }
 
 function clearGallery() {
--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import iziToast from "izitoast";
+import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
 import "izitoast/dist/css/iziToast.min.css";
 
 import { getImagesByQuery } from "./js/pixabay-api";
 import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
 
 const form = document.querySelector(".form");
+const gallery = document.querySelector(".gallery");
 
-const searchInput = document.querySelector(`input[name ="search-text"]`);
+const searchInput = document.querySelector(`input[name="search-text"]`);
 const loader = document.querySelector(".loader");
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 
 form.addEventListener("submit", handleSubmit);
@@ -18,20 +24,33 @@ function handleSubmit(event) {
     event.preventDefault();
     clearGallery();
     showLoader();
-    const serchText = searchInput.value.trim();
+    const searchText = searchInput.value.trim();
     event.target.reset();
-    
-    if (serchText.length > 0) {
-        getImagesByQuery(serchText);
-    }
-    else {
-        iziToast.show({
-                message: 'The field is empty',
+
+    getImagesByQuery(searchText)
+        .then(({ data }) => {
+            if (data.hits.length === 0) {
+                iziToast.show({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    color: 'red',
+                    position: 'topRight',
+                });
+                return;
+            }
+
+            createGallery(data.hits);
+            lightbox.refresh();
+        })
+        .catch(error => {
+            iziToast.show({
+                message: 'Something went wrong!',
                 color: 'red',
-                position: 'topRight',
+            });
+            console.log(error);
+        })
+        .finally(() => {
+            hideLoader();
         });
-        loader.classList.add("hidden");
-    }
-    
 }
+
 
